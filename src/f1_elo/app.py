3c8141f5f751decd3ba67aa59c010fd792b56531
mwtmurphy <mitchell@motorway.co.uk>
2023-11-19 23:58:45 +0000

    feat: pre-1986 data added and weighting dependent on races per year added

--- src/f1_elo/app.py
+++ src/f1_elo/app.py
@@ -2,6 +2,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# app config
+st.set_page_config(layout="wide")
+
 # data import
 @st.cache_data
 def load_data(in_dir: str = "data/prod") -> tuple:
@@ -15,6 +18,7 @@ def load_data(in_dir: str = "data/prod") -> tuple:
 
 gott_df, gott_days = load_data()
 
+# data vis
 st.markdown(
 """
 # F1 Elo
@@ -26,7 +30,7 @@ The question that has ruined many a social event:
 There are many opinions. Is it Fangio, Senna, Michael Schumacher, Hamilton. Let's apply some data 
 science to the problem to see if we can identify - at least - the data GOAT.
 
-For races from 1986 Round 1 - 2023 Brazil, I've created Elo scores (used in chess) for all driver 
+For races from 1950 - 2023 Brazil, I've created Elo scores (used in chess) for all driver 
 inter-team 1v1s due to identical vehicles. This allows to identify the greatest of their time drivers.
 
 ## Method
@@ -39,35 +43,32 @@ increase. If one driver scores 25 and the other 1 however, their increase is sca
 
 ## Results
 
-Taking drivers who were top ranked for at least 16 races (shortest season), this allows me to find
+Taking drivers who were top ranked for 1 season, this allows me to find
 drivers to be considered the GOAT.
 """
 )
 
-fig = px.line(gott_df, x="date", y="score", color="driverId")
-st.plotly_chart(fig)
-st.table(gott_days)
+fig = px.line(gott_df, x="date", y="score", color="driverRef")
+st.plotly_chart(fig, use_container_width=True)
+st.table(gott_days.iloc[:10])
 
 st.markdown(
 f"""
-# The data GOAT = {gott_days['driverId'].iloc[0]}
+# The data GOAT = {gott_days['driverRef'].iloc[0]}
 
 ## Limitations
 
 This approach has clear limitations that are easy to spot:
 
-1. Data begins at 1986, during some drivers careers.
-2. Mechanical failures affect earlier seasons to a larger degree than
-later seasons.
+2. Driver and non-driver retirements aren't accounted for.
 3. Drivers with longer careers have higher scores.
 4. Seasons with more races give those drivers more chances to improve their scores.
 5. Dominant cars give drivers an easier chance to score more points independent of skill.
-6. Not all drivers join the sport with the same skill level, e.g. Hamilton was the F2 champion, Senna
+6. Not all drivers join the sport with the same skill level, e.g. Hamilton was an F2 and F3 champion, Senna
 was a 3x Formula Ford and F3 champion.
 
 ## Next steps
 
-1. Add 1950 - 1985 data for teams with multiple drivers.
 2. Add sprint races.
 3. Add DNFs and type of DNFs (e.g. crashes = penalty, mechanical failures = no penalty).
 4. Scale score change dependent on number of races in a season (less races = less chance to improve).
