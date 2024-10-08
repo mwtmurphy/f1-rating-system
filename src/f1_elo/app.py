23882f7e9e35708217a24e690deb77205fed5940
mwtmurphy <mwtmurphy@gmail.com>
2024-10-08 12:35:44 +0100

    feat: about section and social media links added

--- src/f1_elo/app.py
+++ src/f1_elo/app.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 import streamlit as st
+import st_social_media_links
 
 # app config
 goat_page = st.Page("pages/goat.py", title="Who is the F1 Goat?", url_path="the-goat", icon=":material/star:")
@@ -8,3 +9,16 @@ con_page = st.Page("pages/constructors.py", title="Current constructor ratings",
 
 page_nav = st.navigation([goat_page, dri_page, con_page])
 page_nav.run()
+
+st.sidebar.markdown("""
+    Created by Mitchell Murphy, Lead Data Scientist with over 8 years of experience. See the code 
+    for this project [here](https://github.com/mwtmurphy/f1-rating-system).
+                    
+    If you like this project, see my other projects or send me a message via the links below:
+""")
+social_icons = st_social_media_links.SocialMediaIcons([
+    "https://mwtmurphy.github.io/portfolio/",
+    "https://github.com/mwtmurphy",
+    "https://www.linkedin.com/in/mwtmurphy/"
+])
+social_icons.render(sidebar=True)
